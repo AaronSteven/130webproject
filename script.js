ef3c95f6804f50e3a4837f5bf67d80a230737b9d
Steven Ramos <rsteven46@mail.fresnostate.edu>
2018-10-05 16:46:40 -0700

    commit 56
    here olets go

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+// need to do hover mouse let it glow
+// user can change color of table
+// user can change size of table
+// 
 function addTable() {
     let myTableDiv = document.getElementById("myDynamicTable"); // need a handle to an element   
     let table = document.createElement('TABLE'); // creation of HTML code , notice you dont close the table !
@@ -7,8 +12,8 @@ function addTable() {
 	table.style.borderCollapse='collapse';
     let tableBody = document.createElement('TBODY');
     table.appendChild(tableBody);
-	let sizerow=6;
-	let sizecol=10;
+	let sizerow=7;
+	let sizecol=7;
     for (let i=0; i<sizerow; i++){
        let tr = document.createElement('TR');
        tableBody.appendChild(tr);
@@ -16,7 +21,9 @@ function addTable() {
        for (let j=0; j<sizecol; j++){
            let td = document.createElement('TD');
            td.width='75'; // you can set some elements related to the style directly through the DOM
-           td.appendChild(document.createTextNode("cell (" + i + "," + j + ")"));
+           td.height='75';
+           td.setAttribute("style", "background-color: blue");
+           td.appendChild(document.createTextNode(" "));
            tr.appendChild(td);
        }
     }
