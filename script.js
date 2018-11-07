3391ee0f125592b2efe2baeb670355b43320188f
aaronm-2112 <aaron.marroquin96@gmail.com>
2018-11-06 18:05:38 -0800

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const tableButton = document.getElementById("create");
+const theTimer = document.getElementById("timer");
 
 // need to do hover mouse let it glow
 // user can change color of table
 // user can change size of table
-// 
-function addTable() {
+function addTable(colAndRowSize) {
+    console.log("In add table function");
     let myTableDiv = document.getElementById("myDynamicTable"); // need a handle to an element   
     let table = document.createElement('TABLE'); // creation of HTML code , notice you dont close the table !
 	
@@ -12,8 +14,8 @@ function addTable() {
 	table.style.borderCollapse='collapse';
     let tableBody = document.createElement('TBODY');
     table.appendChild(tableBody);
-	let sizerow=7;
-	let sizecol=7;
+	let sizerow=colAndRowSize;
+	let sizecol=colAndRowSize;
     for (let i=0; i<sizerow; i++){
        let tr = document.createElement('TR');
        tableBody.appendChild(tr);
@@ -29,3 +31,61 @@ function addTable() {
     }
     myTableDiv.appendChild(table); // add the table that was created in the DOM 
 }
+
+function createTable() {
+    //Get input to decide if we are going to build a 7x7 table or 13x13
+    let getRequest = true; 
+    let answer = "";
+    while( getRequest == true) {
+        answer = prompt("Choose a table of either 7x7 or 13x13 by entering 7 or 13");
+        if(answer == "7" || answer == "13") {
+            getRequest = false;
+            
+        } else {
+            alert("Wrong values. Enter a 7 or 13");
+        }
+    }
+    addTable(answer);
+    //temporary 
+    start();
+    
+}
+
+/********************************************This section is for the timer code***********************************************************/ 
+
+
+//timer is the array that we use to do math on in order to update the clock on the game page. We access this array in the runTimer function 
+//minute, second, hundreths
+var timer = [0,0,0,0];
+var timerRunning = false;
+
+//When we do math on the timer array it will not add a leading zero to numbers below 10. This is not how clocks works. This corrects that. 
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
+
+// Run a standard minute/second/hundredths timer
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
+
+
+/***************************************Code to start and reset the game will go in this section*****************************/ 
+
+//This function will be used to start the timer,load the images in place,and anything else that needs to happen when the user decides to begin play 
+function start(){
+    timerInterval = setInterval(runTimer, 10);
+}
+
+
+//Event listeners
+tableButton.addEventListener("click",createTable, false); 
